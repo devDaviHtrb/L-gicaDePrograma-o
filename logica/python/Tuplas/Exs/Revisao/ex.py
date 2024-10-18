397749946a56d1c882b5261da7ff7aa0fa95427d
devDaviHtrb <davideleteribeiro1@gmail.com>
2024-10-18 19:35:28 -0300

    Update ex.py

--- logica/python/Tuplas/Exs/Revisao/ex.py
+++ logica/python/Tuplas/Exs/Revisao/ex.py
@@ -1,7 +1,7 @@
 lista = []#celsius, Fahrenhaint
-while True:
+def tempconvert():
+    for n in lista:print(f"{n[0]}°C são equivalentes a {int(n[0])*9/5+32}°F") if n[1]== "C" else print(f"{n[0]}°F são equivalentes a {(int(n[0])-32)*5/9}°C")
+while "n":
     lista.append(input("Digite a temperatura no seguinte modelo(numero°F/°C): \n").upper().strip().replace("°", " ").split())
     if input("Continuar a adicionar: S/N\n").upper() == "N": break
-def tempconvert():
-    for n in lista:print(f"{n[0]}°C são equivalentes a {int(n[0])*9/5+32}°F") if n[1]== "C" else print(f"{n[0]}°F são equivalentes a {(int(n[0])-32)*5/9}°C") 
 tempconvert()
