8061d347ebc2a6e4b421ef86ad43cff49820d1c4
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-02-28 23:52:02 -0300

    Implementando grafos direcionais e não direcionais

---
+++ Algoritmos/Grafos/GrafosNaoOrdenados.py
@@ -0,0 +1,6 @@
+grafo = {}
+grafo["Principal"] = ["Ad1", "ad2", "ad3"]
+grafo["Ad1"] = ["Principal"]
+grafo["ad2"] = ["Principal"]
+grafo["ad3"] = ["Principal"]
+#As arestas são multidirecionais
---
+++ Algoritmos/Grafos/GrafosOrdenados.py
@@ -0,0 +1,6 @@
+grafo = {}
+grafo["Principal"] = ["Ad1", "ad2", "ad3"]
+grafo["Ad1"] = []
+grafo["ad2"] = []
+grafo["ad3"] = []
+#O principal é vizinho dos "Ad1", "ad2", "ad3", mas os "Ad1", "ad2", "ad3" não possuem nenhum vizinho
